88c543500505a0c842c0e5c38f07c527c31c59c0
Ryan Hall <ryhallry@gmail.com>
2021-01-26 21:53:50 -0800

    init documentation file for utils, edit docs

--- R/utils.R
+++ R/utils.R
@@ -2,12 +2,14 @@
 #'
 #' Socrata data set identifiers are assigned when a data set is created and take
 #' the form of two sets of four alphanumeric characters separated by a dash.
+#' This function tests the format of the identifier, but not whether or not that
+#' identifier actually exists on Socrata.
 #' @param dataset_id A Socrata data set identifier
 #'
 #' @return Boolean. True if the data set identifier meets the format criteria.
+#' False if the provided id does not match the expected format.
 #' @export
 #'
-#' @examples
 validate_dataset_id <- function(dataset_id) {
   if (!is.character(dataset_id)) {
     stop("The provided data set id is not a character vector.")
